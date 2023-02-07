209fcf113b136564ef999d5620033c0b42534ea9
archimed <archimed.shaman@gmail.com>
2023-02-07 16:40:36 +0300

    tmp

--- app/usecase/app.go
+++ app/usecase/app.go
@@ -3,8 +3,12 @@ package usecase
 import (
 	"fmt"
 	"strings"
+	"sync"
 
 	pkgerr "github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/exp/constraints"
+	"morphbits.io/app/usecase/combin"
 )
 
 const (
@@ -31,12 +35,14 @@ type wItem struct {
 	InternalDist int
 }
 
+type WordGroup map[string]*wItem
+
 type App struct {
 	dictReader DictReader
 	calc       DistanceCalculator
 	metrics    Metrics
 
-	filteredWords map[int]map[string]*wItem
+	filteredWords map[int]WordGroup
 }
 
 func NewApp(metrics Metrics, dictReader DictReader, calc DistanceCalculator) *App {
@@ -45,7 +51,7 @@ func NewApp(metrics Metrics, dictReader DictReader, calc DistanceCalculator) *Ap
 		calc:       calc,
 		metrics:    metrics,
 
-		filteredWords: make(map[int]map[string]*wItem),
+		filteredWords: make(map[int]WordGroup),
 	}
 }
 
@@ -58,13 +64,109 @@ func (app *App) Run() error {
 		fmt.Println(key, len(value))
 	}
 
+	distDict := make([]int, 0, len(app.filteredWords))
+	for i := range app.filteredWords {
+		distDict = append(distDict, i)
+	}
+
+	// Now let's find all possible words' length combinations
+	// to get from 20 to 24 password length in sum.
+
+	gen := combin.NewGenerator(distDict, passWords, func(l []int) bool {
+		s := sum(l...)
+		return s >= minPassLength && s <= maxPassLength
+	})
+
+	combinations := make([][]int, 0, 2048)
+	for gen.Next() {
+		combinations = append(combinations, gen.Combination(nil))
+	}
+
+	log.WithField("num", len(combinations)).Info("Possible word length combinations")
+
+	// Now let's find the best word sequences in the each group
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < len(combinations); i++ {
+		wordLengthCombination := combinations[i]
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			pass, err := getBestWords(
+				[4]WordGroup{
+					app.filteredWords[wordLengthCombination[0]],
+					app.filteredWords[wordLengthCombination[1]],
+					app.filteredWords[wordLengthCombination[2]],
+					app.filteredWords[wordLengthCombination[3]],
+				},
+				app.calc,
+			)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(pass)
+		}()
+
+		// for j := 0; j < len(passwords); j++ {
+		// 	fmt.Println(passwords[j])
+		// }
+	}
+
+	wg.Wait()
+
 	return nil
+}
+
+func getBestWords(words [4]WordGroup, calc DistanceCalculator) (*wItem, error) {
+	bestDist := int(^uint(0) >> 1)
+
+	var password *wItem
+
+	for _, word0 := range words[0] {
+		for _, word1 := range words[1] {
+			for _, word2 := range words[2] {
+				for _, word3 := range words[3] {
+					dist01, err := calcWordDistance(word0.Data, word1.Data, calc)
+					if err != nil {
+						return nil, err
+					}
+
+					dist12, err := calcWordDistance(word1.Data, word2.Data, calc)
+					if err != nil {
+						return nil, err
+					}
+
+					dist23, err := calcWordDistance(word2.Data, word3.Data, calc)
+					if err != nil {
+						return nil, err
+					}
+
+					dist := dist01 + dist12 + dist23 +
+						word0.InternalDist + word1.InternalDist + word2.InternalDist + word3.InternalDist
+
+					if dist < int(bestDist) {
+						bestDist = dist
+						password = &wItem{
+							Data:         word0.Data + word1.Data + word2.Data + word3.Data,
+							InternalDist: dist,
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return password, nil
 }
 
 func (app *App) handleWord(rawWord string) error {
 	word := strings.ToLower(rawWord)
 
-	dist, err := app.calcInternalDistance(word)
+	dist, err := calcInternalDistance(word, app.calc)
 	if err != nil {
 		return err
 	}
@@ -75,7 +177,7 @@ func (app *App) handleWord(rawWord string) error {
 	app.metrics.IncWords()
 
 	if m, found := app.filteredWords[length]; !found {
-		app.filteredWords[length] = map[string]*wItem{
+		app.filteredWords[length] = WordGroup{
 			key: {
 				Data:         word,
 				InternalDist: dist,
@@ -101,7 +203,7 @@ func (app *App) handleWord(rawWord string) error {
 	return nil
 }
 
-func (app *App) calcInternalDistance(word string) (int, error) {
+func calcInternalDistance(word string, calc DistanceCalculator) (int, error) {
 	const minWordSize = 2
 
 	if len(word) < minWordSize {
@@ -114,12 +216,25 @@ func (app *App) calcInternalDistance(word string) (int, error) {
 		a := word[i]
 		b := word[i-1]
 
-		d, err := app.calc.GetDistance(a, b)
+		d, err := calc.GetDistance(a, b)
 		if err != nil {
 			return 0, pkgerr.Wrapf(err, "error occurred while calculating distance for word '%s'", word)
 		}
 
 		distance += d
+	}
+
+	return distance, nil
+}
+
+func calcWordDistance(word1, word2 string, calc DistanceCalculator) (int, error) {
+	a := word1[len(word1)-1]
+	b := word2[0]
+
+	distance, err := calc.GetDistance(a, b)
+	if err != nil {
+		return 0, pkgerr.Wrapf(err, "error occurred while calculating distance for words '%s', '%s'",
+			word1, word2)
 	}
 
 	return distance, nil
@@ -136,4 +251,14 @@ func mkKey(word string) string {
 	sb.WriteByte(word[len(word)-1])
 
 	return sb.String()
+}
+
+func sum[A constraints.Ordered](values ...A) A {
+	var sum A
+
+	for i := 0; i < len(values); i++ {
+		sum += values[i]
+	}
+
+	return sum
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.0
+	golang.org/x/exp v0.0.0-20230206171751-46f607a40771
 )
 
 require (
